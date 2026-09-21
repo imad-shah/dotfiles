6c308fec33670c33d7b719b04982c1bef3235270
Imad <imadsshah2004@gmail.com>
2026-09-21 04:10:05 -0400

    better starting size for wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -10,4 +10,8 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+-- default terminal size
+config.initial_rows = 45
+config.initial_cols = 140
+
 return config
